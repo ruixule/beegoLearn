aba66ee1430b5fba515dee885330743ed2710203
ruixule <1414100586@qq.com>
2019-12-30 11:32:50 +0800

    beego框架初始化完成

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "github.com/2017RXL/beegoLearn/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/2017RXL/beegoLearn/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
