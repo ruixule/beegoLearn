27607762668b9c7a78d5d5c587a45d184b3929c4
ruixule <1414100586@qq.com>
2020-06-30 21:38:04 +0800

    优化文件包路径

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	_ "github.com/2017RXL/beegoLearn/routers"
+	_ "beegoLearn/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	beego.Run()
 }
-
--- routers/router.go
+++ routers/router.go
@@ -1,12 +1,12 @@
 package routers
 
 import (
-	"github.com/2017RXL/beegoLearn/controllers"
+	"beegoLearn/controllers"
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 	beego.Router("/editor", &controllers.EditorController{})
 
 }
